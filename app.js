dcb3f58ca0c486870e20fe4b19d6be989e8faa78
bbandeo <bnbandeo@gmail.com>
2020-12-30 17:40:36 -0300

    Agregado EJS

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ let pwVerify = (pwd, hash) => {
 }
 
 // STATIC
-app.use(express.static('static'));
+app.use(express.static('public'));
 
 let connection = mysql.createConnection({
     host: 'localhost',
@@ -39,15 +39,50 @@ connection.connect((err) => {
     console.log("Conectado a base de datos");
 });
 
+
 app.set('view engine', 'ejs');
+
+// // HOME
+
+// index page
+
+
+
 app.get('/', (req, res) => {
     res.locals.user = req.session.user;
-    connection.query('SELECT * from piezas', (error, results, fields) => {
-        if (error) throw error;
-        res.render('index', {
-            title: "Inventario",
-            piezas: results
-        })
-    })
-})
+        res.render('pages/index');
 
+    // connection.query('SELECT * from piezas', (err, results, fields) => {
+    //     if (err) throw err;
+    //     res.render('index', {
+    //         title: "Inventario",
+    //         piezas: results
+    //     });
+    // });
+});
+app.listen(3000)
+// // LOGIN
+// app.get('/login', (req, res) => {
+//     res.render('login', { title: "Login" });
+// });
+// app.post('/login', (req, res) => {
+//     console.log(req.body);
+//     let usr = req.body;
+//     // Cambiar customers por usuarios
+//     connection.query(`SELECT * from usuarios where username="${usr.uname}"`, (err, rows, fields) => {
+//         if (!err) {
+//             console.log('The solution is: ', rows[0]);
+//             if (typeof rows[0] != 'undefined' && pwVerify(usr.pwd, rows[0].password)) {
+//                 console.log("Login exitoso");
+//                 req.session.user = {
+//                     id: rows[0].id,
+//                     username: rows[0].username,
+//                 };
+//                 res.redirect('/');
+//             } else {
+//                 res.render('login', { title: "Login falló. Intenta nuevamente", failed: true });
+//             }
+//         }
+//         else throw err;
+//     });
+// });
